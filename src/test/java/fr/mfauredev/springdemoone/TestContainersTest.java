0fb7c806561aa284f55e5c9512beba0675c553ba
Mathias FAURE <mfauredev@gmail.com>
2023-10-30 17:40:59 +0100

    Testcontainers abstract class

--- src/test/java/fr/mfauredev/springdemoone/TestContainersTest.java
+++ src/test/java/fr/mfauredev/springdemoone/TestContainersTest.java
@@ -1,22 +1,11 @@
 package fr.mfauredev.springdemoone;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-public class TestContainersTest {
+public class TestContainersTest extends AbstractTestcontainers {
 
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("mfauredev-dao-unit-test")
-                    .withUsername("mfauredev")
-                    .withPassword("password");
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
