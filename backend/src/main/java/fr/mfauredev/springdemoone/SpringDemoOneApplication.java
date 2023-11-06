ff9bc910c38bef2293b447a305dd8b6d9ef721ca
Mathias FAURE <mfauredev@gmail.com>
2023-11-06 14:43:29 +0100

    Removed unused import Test CI

--- backend/src/main/java/fr/mfauredev/springdemoone/SpringDemoOneApplication.java
+++ backend/src/main/java/fr/mfauredev/springdemoone/SpringDemoOneApplication.java
@@ -5,7 +5,6 @@ import fr.mfauredev.springdemoone.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import com.github.javafaker.*;
 
